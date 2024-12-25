ee19d5e7c5e1ae5066cbe2bf70ed23ce638c35b2
Marcelo Silveira <mhfs@icloud.com>
2024-12-25 00:19:35 -0300

    drawer overlay

--- app/javascript/controllers/drawer_controller.js
+++ app/javascript/controllers/drawer_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   close() {
+    console.log('close drawer');
     this.element.remove();
     window.history.back();
   }
