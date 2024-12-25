782addda52e5af8762ab8f814286012e9370f41e
Marcelo Silveira <mhfs@icloud.com>
2024-12-25 00:50:19 -0300

    view_component

---
+++ test/components/drawer_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class DrawerComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(DrawerComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
