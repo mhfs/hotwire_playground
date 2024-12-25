760d70c6df0ce1093b202c9195ba87c8fd8af1d6
Marcelo Silveira <mhfs@icloud.com>
2024-12-24 23:02:55 -0300

    bring history back when closing modal

--- app/javascript/controllers/drawer_controller.js
+++ app/javascript/controllers/drawer_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   close() {
-    this.element.remove(); // Remove the drawer from the DOM
+    this.element.remove();
+    window.history.back();
   }
 }
