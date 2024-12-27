829a38b8e1fd50702a488ed39542574ac894baa9
Marcelo Silveira <mhfs@icloud.com>
2024-12-26 22:51:03 -0300

    close on ESC

--- app/javascript/controllers/drawer_controller.js
+++ app/javascript/controllers/drawer_controller.js
@@ -1,6 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  connect() {
+    document.addEventListener("keydown", this.handleKeydown);
+  }
+
+  disconnect() {
+    document.removeEventListener("keydown", this.handleKeydown);
+  }
+
+  handleKeydown = (event) => {
+    if (event.key === "Escape" || event.keyCode === 27) {
+      this.close();
+    }
+  };
+
   close() {
     console.log('close drawer');
     this.element.remove();
