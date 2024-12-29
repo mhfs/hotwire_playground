e8571a548bef3d810d98b00a4f18f8b3f72fb160
Marcelo Silveira <mhfs@icloud.com>
2024-12-29 18:59:43 -0300

    push url state sooner

--- app/javascript/controllers/drawer_trigger_controller.js
+++ app/javascript/controllers/drawer_trigger_controller.js
@@ -8,22 +8,28 @@ export default class extends Controller {
   open(e) {
     // Prevent the link navigation as we're going to handle it with Turbo below.
     e.preventDefault();
+
+    // Clone the drawer template with proper setup into the DOM.
     this.initDrawerFrame();
+
+    // Do this last so that frame initializes with previous url
+    if (this.advanceValue) window.history.pushState({}, null, this.element.href);
   }
 
   initDrawerFrame() {
-    // Clone the drawer frame template
+    // Clone the drawer frame template.
     let content = document.querySelector('template#drawer-template').content.cloneNode(true);
 
-    // Pass the previous URL to the drawer for the inner controller to use on close
+    // Pass the previous URL to the drawer for the inner controller to use on close.
     let container = content.querySelector('.drawer');
     let previousUrl = window.location.pathname + window.location.search;
     container.dataset.drawerPreviousUrlValue = previousUrl;
 
-    // If advance was set on the link, set the turbo-frame to advance
+    // If advance was set on the link, set the turbo-frame to advance.
     if (this.advanceValue) {
       let frame = content.querySelector('turbo-frame');
-      frame.dataset.turboAction = 'advance';
+      // Use replace instead of advance because we already pushed on `open` above.
+      frame.dataset.turboAction = 'replace';
     }
 
     // Append the drawer to the body with the loading state so that user
