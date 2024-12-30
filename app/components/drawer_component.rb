8536609fcd2709eae2a621affc9aa05fb477d387
Marcelo Silveira <mhfs@icloud.com>
2024-12-29 23:31:10 -0300

    move previous url to business component

--- app/components/drawer_component.rb
+++ app/components/drawer_component.rb
@@ -3,15 +3,16 @@
 class DrawerComponent < ViewComponent::Base
   include Turbo::FramesHelper
 
-  def initialize(title:)
+  def initialize(title:, previous_url:)
     @title = title
+    @previous_url = previous_url
   end
 
   def wrap_in_turbo_frame(&block)
     if helpers.turbo_frame_request?
       turbo_frame_tag "drawer-frame", &block
     else
-      render DrawerFrameComponent.new(previous_url: animals_path) do
+      render DrawerFrameComponent.new(previous_url: @previous_url) do
         capture(&block)
       end
     end
